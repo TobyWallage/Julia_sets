fbedbf5f09fa9999c54f68e61dccf89173cfb989
Toby Wallage <tlw31@sussex.ac.uk>
2023-08-08 13:26:36 +0100

    did stuffs

--- py_fractal/Test/test.py
+++ py_fractal/Test/test.py
@@ -1,7 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from py_fractal import gen_julia_fractal
-
-fractal = gen_julia_fractal(1000, 2000, 2, -0.74543+0.11301j, 1000)
+print("Generating Fractal")
+fractal = gen_julia_fractal(20000, 10000, 2, -0.74543+0.11301j, 1000)
+print("Fractal Generated")
 plt.imshow(fractal)
 plt.show()
+print("Done")
