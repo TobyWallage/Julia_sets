fe19585678c0b18adc374437c5cafe8c7ae20511
Toby Wallage <tlw31@sussex.ac.uk>
2023-08-09 19:46:20 +0100

    Separated Commandline and python Module

--- py_fractal/Test/test.py
+++ py_fractal/Test/test.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from py_fractal import gen_julia_fractal
 print("Generating Fractal")
-fractal = gen_julia_fractal(20000, 10000, 2, -0.74543+0.11301j, 1000)
+fractal = gen_julia_fractal(2000, 1000, 2, -0.74543+0.11301j, 1000)
 print("Fractal Generated")
 plt.imshow(fractal)
 plt.show()
